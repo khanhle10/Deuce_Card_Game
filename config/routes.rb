34ce5d2571c38bc4d49b9c19b2481b12953ea536
mykevin81 <mykevin81@gmail.com>
2015-11-25 17:42:25 -0800

    rescope devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -32,7 +32,7 @@ Rails.application.routes.draw do
     delete 'signout' => 'devise/sessions#destroy', :as => :destroy_user_session
   end
 =end
-  
+
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
