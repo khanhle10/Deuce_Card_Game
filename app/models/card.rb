67ac065240f5fd8af95610fee22946c901345935
khanhle10 <khanhle10@hotmail.com>
2015-12-02 00:50:12 -0800

    modified the routes to game page where both chat and game runs at the same time

--- app/models/card.rb
+++ app/models/card.rb
@@ -3,7 +3,7 @@ class Card < ActiveRecord::Base
   VALUE_WEIGHT = Hash[%w(3 4 5 6 7 8 9 10 J Q K A 2).zip((1..13).to_a)]
   SUIT_WEIGHT = Hash[%w(spade club diamond heart).zip((0..3).to_a)]
 
-  #attr_accessible :suit, :value
+  attr_accessible :suit, :value
 
   has_many :player_cards, :dependent => :destroy
 
@@ -22,20 +22,6 @@ class Card < ActiveRecord::Base
     SUIT_WEIGHT[suit]
   end
 
-  def is_a_heart
-    suit == "heart"
-  end
 
-  def is_a_spade
-    suit = "spade"
-  end
-
-  def is_a_club
-    suit = "club"
-  end
-
-  def is_a_diamond
-    suit = "diamond"
-  end
 
 end
