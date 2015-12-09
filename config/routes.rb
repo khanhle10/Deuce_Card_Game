140cd1e769000b9ac30377f831f1ab3b1135e160
khanhle10 <khanhle10@hotmail.com>
2015-12-09 15:21:19 -0800

    remove lobby db, and all lobby associated files as well as changes to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
 
   resources :deuces
-  resources :lobbies
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
--- db/migrate/20151206103401_create_lobbies.rb
+++
@@ -1,9 +0,0 @@
-class CreateLobbies < ActiveRecord::Migration
-  def change
-    create_table :lobbies do |t|
-      t.integer :game_id
-
-      t.timestamps null: false
-    end
-  end
-end
