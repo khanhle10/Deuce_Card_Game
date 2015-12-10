838378d78a3afac4bef7c887b8c404869ca20ab2
sjnasr <sjnasr18@yahoo.com>
2015-12-09 21:26:45 -0800

    My machine added <<<<< head and master.  Removed those

--- app/assets/javascripts/deuce.js
+++ app/assets/javascripts/deuce.js
@@ -1,9 +1,8 @@
-<<<<<<< HEAD
+
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 // You can use CoffeeScript in this file: http://coffeescript.org/
-=======
->>>>>>> master
+
 var ROOM_CHANNEL = "deuce_";
 var PLAYER_JOINED = "playerJoined";
 var ROOM_CREATED = "roomCreated";
