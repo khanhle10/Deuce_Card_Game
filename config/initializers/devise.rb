3651e675eeafdefec70901e9e205af20b256abdd
khanhle10 <khanhle10@hotmail.com>
2015-12-15 19:20:42 -0800

    add pusher key

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -6,6 +6,7 @@ Devise.setup do |config|
   # confirmation, reset password and unlock tokens in the database.
   # Devise will use the `secret_key_base` on Rails 4+ applications as its `secret_key`
   # by default. You can change it below and use your own secret key.
+  config.secret_key = '433bc041ae4892070d67ad176ad987c51b654a0102225f5a74568f61b1a279321f0beac3d0b3124019cbedea735d38bfd53fb2e72626b9a78b907703956d0db3'
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
